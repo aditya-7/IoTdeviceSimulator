6e579d10d4ed3978c79460a61b98966b6bb753cd
aditya7 <aditya.kishore@lnttechservices.com>
2019-09-23 11:36:48 +0530

    aditya--[WIP]

--- simulate_device.js
+++ simulate_device.js
@@ -3,50 +3,65 @@ const config = JSON.parse(fs.readFileSync("config.1.json"));
 const register_device = require("./register_device");
 const send_message = require("./send_message");
 const async = require("async");
+const _ = require("lodash");
 
 function simulate(deviceId, callback) {
 
     function simulate_attributes() {
         return {
-            "deviceId": deviceId,
-            "timestamp": Date.now()
+            "macId": deviceId,
+            "tstampval": Date.now(),
+            "energy": (Math.random() * (0.0 - 1.0) + 1.0).toFixed(4)
         };
     }
 
-    register_device(config, deviceId, function (err) {
-        if (err) {
-            console.error(err);
-            return
-        }
-        setInterval(function () {
-            const attributes = simulate_attributes();
-            send_message(config, deviceId, JSON.stringify(attributes), function (err) {
-                if (err) {
-                    console.error(err);
-                    return
-                }
-                console.log("Message sent to IoTHub");
-                console.log("device= " + deviceId);
-                console.log("message= ", attributes);
-            });
-        }, /*1 * 60 * */ 60 * 1000);
+    setInterval(function () {
+        const attributes = simulate_attributes();
+        send_message(config, deviceId, JSON.stringify(attributes), function (err) {
+            if (err) {
+                console.error(err);
+                return
+            }
+            console.log("Message sent to IoTHub");
+            console.log("device= " + deviceId);
+            console.log("message= ", attributes);
+        });
+    }, config["message-interval-minutes"] * 60 * 1000);
+}
+
+function register_devices(devices) {
+    _.forEach(devices.devices, deviceId => {
+        register_device(config, deviceId, function (err) {
+            if (err) {
+                console.error(err);
+                return
+            }
+        });
     });
 }
 
-function create_devices(n) {
+function create_devices(devices) {
     const devices_simulation = [];
-    for (var i = 1; i <= n; i++) {
-        var j = i;
-        if (i < 10) j = "0000" + i;
-        else if (i < 100) j = "000" + i;
-        else if (i < 1000) j = "00" + i;
-        else j = "0" + i;
-        devices_simulation.push(async.apply(simulate, "energyconsmptiondev" + j));
+    for (var i = 1; i <= devices.length; i++) {
+        devices_simulation.push(async.apply(simulate, devices[i]));
     }
     return devices_simulation;
 }
 
-const devices_simulation = create_devices(100);
-async.parallel(devices_simulation);
+fs.readFile('./devices.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+    var devices = JSON.parse(data);
+    switch (process.argv[2]) {
+        case "REGISTER":
+            register_devices(devices);
+            break;
+        case "SIMULATE":
+            const devices_simulation = create_devices(config["number-of-devices"]);
+            async.parallel(devices_simulation);
+            break;
+        default:
+            console.log("Please provide valid argument");
+    }
+});
 
 process.stdin.resume();
